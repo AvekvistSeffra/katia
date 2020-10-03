4086ee60c59eb5d0f9ca0f3cbc8ea45c72338122
Hampus Avekvist <seffraavekvist@gmail.com>
2020-10-03 02:54:35 +0200

    Updated to add debug mode for IP choices, or whatever.

--- client/client.py
+++ client/client.py
@@ -1,12 +1,24 @@
 import asyncio
 import websockets
+import socket
+
+debug = True
 
 async def hello(uri, name):
     async with websockets.connect(uri) as websocket:
         await websocket.send(name)
         print(await websocket.recv())
 
-def main(uri):
+def main():
+    if debug:
+        ip = socket.gethostbyname(socket.gethostname())
+        port = 8765
+    else:
+        ip = input("Enter an IP Address: ")
+        port = input("Enter a port number: ")
+
+    uri = "ws://{}:{}".format(ip, port)
+
     name = input("Enter your name: ")
     call = hello(uri, name)
     
@@ -16,4 +28,4 @@ def main(uri):
         print("Byebye. ")
 
 if __name__ == "__main__":
-    main("ws://192.168.1.10:8765")
+    main()
--- server/server.py
+++ server/server.py
@@ -1,5 +1,8 @@
 import asyncio
 import websockets
+import socket
+
+debug = True
 
 async def echo(websocket, path):
     name = await websocket.recv()
@@ -10,7 +13,16 @@ async def echo(websocket, path):
     await websocket.send(message)
 
 def main():
-    start_server = websockets.serve(echo, "192.168.1.10", 8765)
+    ip = socket.gethostbyname(socket.gethostname())
+
+    if debug:
+        port = 8765
+    else:
+        port = input("Enter a port number: ")
+
+    print("Server starts with address {}:{}".format(ip, port))
+
+    start_server = websockets.serve(echo, ip, port)
 
     try:
         asyncio.get_event_loop().run_until_complete(start_server)
