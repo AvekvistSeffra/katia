e3b9dea20a674f2f78219797100f611001d40da4
Edvin Avekvist <seffraavekvist@gmail.com>
2020-10-07 01:58:45 +0200

    Revert "Build a failing GUI but it's at least something and it is Electron after all so it's fine, will work on this some other day."
    This reverts commit 774300444763d31ccd1b6e54a2066a947afaed2e.

--- client/client.py
+++ client/client.py
@@ -1,7 +1,6 @@
 import asyncio
 import websockets
 import socket
-import sys
 
 debug = True
 
@@ -9,7 +8,6 @@ async def hello(uri, name):
     async with websockets.connect(uri) as websocket:
         await websocket.send(name)
         print(await websocket.recv())
-        sys.stdout.flush()
 
 def main():
     if debug:
@@ -28,9 +26,6 @@ def main():
         asyncio.get_event_loop().run_until_complete(call)
     except KeyboardInterrupt:
         print("Byebye. ")
-        sys.stdout.flush()
 
 if __name__ == "__main__":
-    print("Hello, world!")
-    sys.stdout.flush()
     main()
